2f4bf0b68a45f95bdc2c3566025841602e19da20
Ali Oğuzhan YÜCE <91226446+aliousyucex@users.noreply.github.com>
2024-01-17 19:26:56 +0300

    :burn: Remove the chain settings

--- rarity.js
+++ rarity.js
@@ -1,66 +1,19 @@
 const basePath = process.cwd()
 const fs = require("fs")
 const path = require("path")
-const axios = require("axios").default
-require('dotenv').config()
-const TIMEOUT = 500
-const CONTRACT_ADDRESS = '0x9A4d952Ff903D6ff19112Af8C38E243a36ae4ded' // CodeCats NFT Collection
-const CHAIN = 'polygon'
 
 const getRarity = async () => {
   const nfts = []
-  let page_number = 1
 
-  const first_page = await getNftData(CONTRACT_ADDRESS, page_number)
-  const total_pages = first_page.total
-  const last_page = Math.ceil(total_pages / 50)
-  page_number++
-  nfts.push(...first_page.nfts)
-  
-  while(page_number <= last_page) { 
-    await timer(TIMEOUT)
-    const page_data = await getNftData(CONTRACT_ADDRESS, page_number)
-    page_number++
-    nfts.push(...page_data.nfts)
   }
+
   processRarity(nfts)
-}
-
-function timer(ms) {
-  return new Promise(res => setTimeout(res, ms))
-}
-
-function getNftData(contract_address, page_number) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      method: 'GET',
-      url: `https://api.nftport.xyz/v0/nfts/${contract_address}`,
-      params: {
-        chain: CHAIN,
-        page_number: page_number,
-        include: 'all',
-        refresh_metadata: 'true'
-      },
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: process.env.NFTPORT_API_KEY
-      }
-    }
-
-    axios.request(options).then(function (response) {
-      console.log(`Recieved Page ${page_number}`)
-      resolve(response.data)
-    }).catch(function (error) {
-      console.error(error)
-      reject(error)
-    })
-  })
 }
 
 function processRarity(nfts) {
   console.log('Processing Rarity')
   const rarity = {}
-  
+
   // loop through all nfts
   for(const nft of nfts) {
     // check if attributes exist
